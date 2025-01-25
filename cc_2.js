b7baa93832d78d4e6cf8ed5832134a866a2ae2e9
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-01-25 17:03:55 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -6,3 +6,15 @@ let products = ["bed", "vanity", "mirror",
 
     products.pop ;                          // Deleting the new element from the array
  console.log("Products:", products) ;   // Output: Products: (5) ['bed', 'vanity', 'mirror', 'desk', 'chair']
+
+
+// Task 2 - Student Scores
+
+let scores = [99, 95, 90, 89, 87, 86] ;     // Creation of an array
+    scores.splice(1,1,96)                   // Changing the 2nd score
+
+    let scoresTotal = scores.reduce((sum, score) => sum + score, 0) ;  // Calculating the sum of the scores to find the average
+    let scoresAverage = scoresTotal / scores.length   // Calcultion of the scores' average
+
+console.log("Scores:", scores)              // Output: Scores: (6) [99, 96, 90, 89, 87, 86]
+console.log("Scores' average:", scoresAverage)  // Output: Scores' average 91.16666666666667
