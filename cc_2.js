7cba957980356e95a62373f960d6a802cec0b170
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-01-25 16:11:58 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+// Task 1 - Store Inventory
+
+let products = ["bed", "vanity", "mirror", 
+    "desk", "chair"] ;                      // Creation of the array
+    products.push("wall decor") ;           // Adding a new element to the array
+
+    products.pop ;                          // Deleting the new element from the array
+ console.log("Products:", products) ;   // Output: Products: (5) ['bed', 'vanity', 'mirror', 'desk', 'chair']
