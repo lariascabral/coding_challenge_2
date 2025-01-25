8f4bc068fa56963cc60f7fec014a816a3b224d63
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-01-25 17:57:17 -0500

    Task 4 - Customer Database

--- cc_2.js
+++ cc_2.js
@@ -11,21 +11,49 @@ let products = ["bed", "vanity", "mirror",
 // Task 2 - Student Scores
 
 let scores = [99, 95, 90, 89, 87, 86] ;     // Creation of an array
-    scores.splice(1,1,96)                   // Changing the 2nd score
+    scores.splice(1,1,96) ;                 // Changing the 2nd score
 
     let scoresTotal = scores.reduce((sum, score) => sum + score, 0) ;  // Calculating the sum of the scores to find the average
-    let scoresAverage = scoresTotal / scores.length   // Calcultion of the scores' average
+    let scoresAverage = scoresTotal / scores.length ;   // Calcultion of the scores' average
 
-console.log("Scores:", scores)              // Output: Scores: (6) [99, 96, 90, 89, 87, 86]
-console.log("Scores' average:", scoresAverage)  // Output: Scores' average 91.16666666666667
+console.log("Scores:", scores) ;            // Output: Scores: (6) [99, 96, 90, 89, 87, 86]
+console.log("Scores' average:", scoresAverage) ;  // Output: Scores' average 91.16666666666667
 
 
 // Task 3 - Employee Records
 
 let employee = { name: "Rose Cabrera", age: 24, 
-    department: "HR", isActive: true } ;     //Creation of an employee profile
+    department: "HR", isActive: true } ;     // Creation of an employee profile
 
-employee.department = "Marketing"           //Modifing an existing property in the object
-employee.position = "Technical Manager"     //Adding a new property to the object
+employee.department = "Marketing" ;          // Modifing an existing property in the object
+employee.position = "Technical Manager" ;    // Adding a new property to the object
 
-console.log(employee)
+console.log("Employee record:", employee) ;  // Output: Employee record: {name: 'Rose Cabrera', age: 24, department: 'Marketing', isActive: true, position: 'Technical Manager'}
+
+
+// Task 4 - Customer Database
+
+customers = [
+{
+    name: "Lincoln Alford", 
+    email: "alford@gmail.com", 
+    purchaseAmount: 1234.5
+}, 
+{
+    name: "Brody Andrews",
+    email: "bigandrews@hotmail.com",
+    purchaseAmount: 987.6
+},
+{
+    name: "Kenneth Almond",
+    email: "almondkenneth@outlook.com",
+    purchaseAmount: 1928
+}] ;                                      // Creation of the array known as customer database
+
+customers[3] = {
+    name: "LeighAnn Apricot",
+    email: "leighaapricot345@gmail.com",
+    purchaseAmount: 100.99
+} ;                                      // Adding a new object to the array
+
+console.log("Customer Database:", customers)    // Output: Customer Database: (4) [{…}, {…}, {…}, {…}]
